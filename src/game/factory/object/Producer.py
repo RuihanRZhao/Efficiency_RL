ed0ed073ca9c57188477ba2d98f18ed7fe311a6d
RyenBZhao <fouxzhao@GMAIL.COM>
2023-08-30 18:04:04 +0800

    add comments for producer

--- src/game/factory/object/Producer.py
+++ src/game/factory/object/Producer.py
@@ -9,6 +9,11 @@ from src.game.factory.tool_data.t_sql import SQL
 
 class Producer(object):
     def __init__(self, element: dict):
+        """
+        Initialize a Producer object.
+
+        :param element: Initial configuration data for the producer.
+        """
         self.un_id = ""
         self.material_list = []
         self.material_amount = []
@@ -20,6 +25,11 @@ class Producer(object):
         self.initialize()
 
     def __repr__(self):
+        """
+        Return a string representation of the Producer object.
+
+        :return: String representation of the object.
+        """
         return (
             f"{self.name}[{self.un_id}]\n"
             f"Origin Inventory: {self.inventory}  |  Capability of Inventory: {self.inventory_cap}\n"
@@ -28,6 +38,11 @@ class Producer(object):
         )
 
     def initialize(self) -> bool:
+        """
+        Initialize the Producer object with default values.
+
+        :return: True if initialization is successful, else False.
+        """
         self.un_id = ""
         self.material_list = []
         self.material_amount = []
@@ -38,6 +53,15 @@ class Producer(object):
     def reset(self) -> bool:
         """
         Reset the Producer's properties to their initial values.
+
+        :return: True if reset is successful, else False.
         """
         return self.initialize()
 
+    def produce(self) -> tuple:
+        """
+        Produce items using the Producer.
+
+        :return: A tuple containing production results.
+        """
+        return None, None
