f08b8910e225e1f505cad39b17a2616366275ff1
Ruihan Zhao <ruihan.ryen.zhao@gmail.com>
2023-09-04 13:40:45 +0800

    modify the init file and package necessary functions and classes for outer port

---
+++ src/game/factory/object/__init__.py
@@ -0,0 +1,9 @@
+from .Material import Material as Material
+from .Producer import Producer as Producer
+
+# version
+__version__ = '2.0.2'
+
+print(__name__, "\t", __version__)
+
+
---
+++ src/game/factory/tool_data/__init__.py
@@ -0,0 +1,8 @@
+from .t_sql import SQL
+from .t_csv import CSV
+# version
+__version__ = '2.0.1'
+
+print(__name__, "\t", __version__)
+
+
